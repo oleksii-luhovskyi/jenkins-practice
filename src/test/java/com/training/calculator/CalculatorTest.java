63811eee4cb9fd4a03549b61bcc431fadcde9b20
oleksii-luhovskyi <oleksii.luhovskyi@gmail.com>
2024-07-08 16:45:50 +0300

    Update CalculatorTest.java

--- src/test/java/com/training/calculator/CalculatorTest.java
+++ src/test/java/com/training/calculator/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
         assertEquals(-1, calculator.add(2, -3));
         assertEquals(0, calculator.add(0, 0));
     }
-
+// Some change
     @Test
     public void testSubtract() {
         assertEquals(-1, calculator.subtract(2, 3));
